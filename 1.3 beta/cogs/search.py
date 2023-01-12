1cde47af5cf60a4c3f0d5e91cb3a156311e35f2d
IvanTopGaming <zjarc0@mai.ru>
2023-01-12 18:02:36 +0300

    new file:   1.3 beta/ImageParser.py new file:   1.3 beta/cogs/search.py

---
+++ 1.3 beta/cogs/search.py
@@ -0,0 +1,23 @@
+import disnake
+from disnake.ext import commands
+from ImageParser import *
+
+parser = YandexImage()
+
+class search(commands.Cog):
+    def __init__(self, bot: commands.Bot):
+        self.bot = bot
+            
+    @commands.slash_command(description = "Ищи любые картинки.")
+    async def search(inter, search: str):
+        for item in parser.search(search):
+            print(item.preview.url)
+            await inter.response.send_message(item.preview.url)
+            break
+            
+            
+
+
+
+def setup(bot: commands.Bot):
+    bot.add_cog(search(bot))
--- 1.3 beta/config.py
+++ 1.3 beta/config.py
@@ -4,4 +4,4 @@ test_guilds=(
 reload=True
 dev_id=712631530002055169
 black_guilds=[]
-token='MTAzODA2NTg2NjcyMzU3NzkxNg.Gt4VY6.iqpB5JOMIe6HzciIVUEgKeRCnWHx-M_D6ECGHg0'
+token='MTAzODA2NTg2NjcyMzU3NzkxNg.Gt4VY6.iqpB5JOMIe6HzciIVUEgKeRCnWHx-M_D6ECGHg'
