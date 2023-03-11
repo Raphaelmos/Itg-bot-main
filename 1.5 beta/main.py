89209980450afe8a7f8a3f3a5922290e784b0285
IvanTopGaming <zjarc0@mail.ru>
2023-03-11 11:24:38 +0300

    Bux fix

--- 1.5 beta/main.py
+++ 1.5 beta/main.py
@@ -20,26 +20,7 @@ print("Cogs is loaded...")
 async def on_ready():
 	print(f'Logged on as {bot.user}!')
 	print("Бот готов!")
-	await bot.change_presence(status=disnake.Status.online, activity=disnake.Game("коги!")
-
-@bot.slash_command(description="Выгрузить коги")
-async def unload(inter, extension):
-	if inter.author.id == dev_id:
-		bot.unload_extension(f"cogs.{extension}")
-		print(f"Cog '{extension}' was unloaded")
-		await inter.send(f"Ког **{extension}** выгружен.", ephemeral=True)
-	else:
-		await inter.send("Вы не имеете право использовать данную команду", ephemeral=True)
-
-
-@bot.slash_command(description="Загрузить коги")
-async def load(inter, extension):
-	if inter.author.id == dev_id:
-		bot.load_extension(f"cogs.{extension}")
-		print(f"Cog '{extension}' was loaded")
-		await inter.send(f"Ког **{extension}** загружен.", ephemeral=True)
-	else:
-		await inter.send("Вы не имеете право использовать данную команду", ephemeral=True)
+	await bot.change_presence(status=disnake.Status.online, activity=disnake.Game("коги!"))
 
 @bot.event
 async def on_guild_join(guild):
